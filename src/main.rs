b207d7b6ddc03c6f3eebe01263a0abac7c7e1e93
Sebastian Paarmann <sebastian.paarmann@gmail.com>
2020-05-28 22:05:17 +0200

    Add serenity, very basic bot framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use serenity::client::Client;
+use serenity::framework::standard::{
+    macros::{command, group},
+    CommandResult, StandardFramework,
+};
+use serenity::model::channel::Message;
+use serenity::prelude::{Context, EventHandler};
+use std::env;
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+impl EventHandler for Handler {}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting bot...");
+
+    let mut client = Client::new(&env::var("PROBLEM_CHILD_TOKEN").expect("token"), Handler)
+        .expect("Error creating client");
+    client.with_framework(
+        StandardFramework::new()
+            .configure(|c| c.prefix("!"))
+            .group(&GENERAL_GROUP),
+    );
+
+    if let Err(why) = client.start() {
+        println!("An error occured while running the client: {:?}", why);
+    }
+}
+
+#[command]
+fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!")?;
+
+    Ok(())
 }
